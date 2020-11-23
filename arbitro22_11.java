4a97ff68f79b80436faee2904412bb817ab60618
giovani3109 <69610026+giovani3109@users.noreply.github.com>
2020-11-22 23:09:23 -0300

    Create arbitro22_11.java

---
+++ arbitro22_11.java
@@ -0,0 +1,20 @@
+package giovani_bergamo;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Arbitro extends Equipe {
+    private String posicaoArbitro;
+
+    public Arbitro() {
+        super();
+    }
+
+    public String getPosicaoArbitro() {
+        return posicaoArbitro;
+    }
+
+    public void setPosicaoArbitro(String posicaoArbitro) {
+        this.posicaoArbitro = posicaoArbitro;
+    }
+}
